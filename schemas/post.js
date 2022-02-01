4a44a85af901efd3d75a04283d5690bbc7783ba6
Rayen Labidi <89748200+IamRayen@users.noreply.github.com>
2022-02-01 16:34:42 +0100

    introduction added to schema

--- schemas/post.js
+++ schemas/post.js
@@ -18,6 +18,11 @@ export default {
       },
     },
     {
+      name: 'introduction',
+      title: 'Introduction',
+      type: 'text',
+    },
+    {
       name: 'author',
       title: 'Author',
       type: 'reference',
