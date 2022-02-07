383e7fab5e53a4cc81a6db6fbb8ebc001b01532d
Rayen Labidi <89748200+IamRayen@users.noreply.github.com>
2022-02-07 16:17:48 +0100

    added painting...

---
+++ schemas/painting.js
@@ -0,0 +1,68 @@
+export default {
+    name: 'painting',
+    title: 'Painting',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        options: {
+          source: 'title',
+          maxLength: 96,
+        },
+      },
+      {
+        name: 'introduction',
+        title: 'Introduction',
+        type: 'text',
+        rows: 3,
+        validation: Rule => Rule.max(120)
+      },
+      {
+        name: 'author',
+        title: 'Author',
+        type: 'reference',
+        to: {type: 'author'},
+      },
+      {
+        name: 'mainImage',
+        title: 'Main image',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+      {
+        name: 'categories',
+        title: 'Categories',
+        type: 'array',
+        of: [{type: 'reference', to: {type: 'category'}}],
+      },
+      {
+        name: 'publishedAt',
+        title: 'Published at',
+        type: 'datetime',
+      },
+    ],
+  
+    preview: {
+      select: {
+        title: 'title',
+        author: 'author.name',
+        media: 'mainImage',
+      },
+      prepare(selection) {
+        const {author} = selection
+        return Object.assign({}, selection, {
+          subtitle: author && `by ${author}`,
+        })
+      },
+    },
+  }
+  
